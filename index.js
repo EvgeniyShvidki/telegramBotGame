8b849cfef2c217916347d91a3aa7e9a3fe979e0c
EvgeniyShvidki <zheka.shvudki@gmail.com>
2024-03-08 18:47:52 +0200

    first commit

--- index.js
+++ index.js
@@ -1,61 +1,95 @@
 import TelegramBot from "node-telegram-bot-api";
 import { shuffleEmoji, getEmoji } from "./shuffle-emoji.js";
+import fs from "fs";
+import { levelKeyboard } from "./keybords.js";
+import { recordsKeyboard } from "./keybords.js";
 
-let emojiKeyboard = {}
+let numMessage = -1;
+let emojiKeyboard = {};
+let amountCards = 3;
 
-let bot = new TelegramBot("6937853671:AAGT9Boa97ztrgOQzfv9aQOTLEfq4tjTfSs", {polling: true});
 let chatId = 0;
+let nameUser = "";
+let startTimeGame = 0;
+let bot = new TelegramBot("6937853671:AAGT9Boa97ztrgOQzfv9aQOTLEfq4tjTfSs", {
+  polling: true,
+});
+//  npm i @types/node-telegram-bot-api
+
 bot.setMyCommands([
+  { command: "level", description: "change level" },
   { command: "start", description: "start game" },
   { command: "username", description: "show username" },
+  { command: "records", description: "show user time records" },
 ]);
 
-let finishList = []
+let finishList = [];
 
-// game: 
+let records = [];
+function getRecords() {
+  records = JSON.parse(fs.readFileSync("./records.json").toString());
+}
 
-// 🌶🍊🥕🫐🥬🍋😂
-let emoji = ["🌶", "🍊", "😂", "🍋", "🥬", "🥕"];
+function saveRecord() {
+  fs.writeFile("records.json", JSON.stringify(records), () => {});
+}
 
+getRecords();
+console.log(records);
+console.log(typeof records);
 
-let unblockList = []
+// game:
+// potok 10s  +10s 2min wait
+// buffer pot + pot + pot [34, 56, 23, 32, 100].toString()
 
+// 🌶🍊🥕🫐🥬🍋😂🍓🫑🍎🥭🥑🥥🌹🍇🌮🍠🥶🍕🍔🍌
+let emoji = [
+  "🌶",
+  "🍊",
+  "😂",
+  "🍋",
+  "🥬",
+  "🥕",
+  "🍓",
+  "🫑",
+  "🍎",
+  "🥭",
+  "🥑",
+  "🥥",
+  "🌹",
+  "🍇",
+  "🍠",
+  "🌮",
+  "🍌",
+  "🥶",
+  "🍕",
+  "🍔",
+  "🍙",
+];
+
+let unblockList = [];
 
 // 6 btn: 3 fruit
 
-
-
-function randomKeyboard(index, emoji_){
-
-
-
-
-  let oldEmoji = ""
+function randomKeyboard(index, emoji_) {
+  let oldEmoji = "";
   let newEmoji = emoji_;
 
-
-
-
-
-
-
-
-
-
   if (emojiKeyboard.reply_markup != undefined) {
-    let keyboardOld = emojiKeyboard.reply_markup.inline_keyboard.flat()
+    let keyboardOld = emojiKeyboard.reply_markup.inline_keyboard.flat();
+    console.log(keyboardOld.length);
     for (let i = 0; i < keyboardOld.length; i++) {
-      if (keyboardOld[i].text != '⬜️' && !unblockList.includes(keyboardOld[i].text)) {
+      console.log(keyboardOld[i].text);
+      if (
+        keyboardOld[i].text != "⬜️" &&
+        !unblockList.includes(keyboardOld[i].text)
+      ) {
         oldEmoji = keyboardOld[i].text;
-        break
+
+        break;
       }
     }
   }
-
-
-  
-
-
 
   console.log("oldEmoji", oldEmoji);
   console.log("newEmoji", newEmoji);
@@ -64,13 +98,7 @@ function randomKeyboard(index, emoji_){
     unblockList.push(oldEmoji);
   }
 
-
-
-
-  let amountCards = 4
-  if (index && emoji_) {
-    console.log('wow')
-  } else {
+  if (!emoji_) {
     let newListEmoji = shuffleEmoji(emoji);
     let readyList = getEmoji(newListEmoji, amountCards);
     finishList = shuffleEmoji(readyList);
@@ -78,100 +106,118 @@ function randomKeyboard(index, emoji_){
 
   console.log(finishList);
 
-  let btns = [];
-  let btns2 = [];
+  let rows = [];
+  let amountCardsInRow = 5;
+  if (amountCards < 5) {
+    amountCardsInRow = amountCards;
+  }
 
-  
+  // 8 === '8'
 
- 
+  for (let i = 0; i < finishList.length; i++) {
+    if (i % amountCardsInRow === 0) {
+      rows.push([]);
+    }
+    console.log(i, index, "i index");
 
-
-  for (let i = 0; i < amountCards; i++) {
-    if (i  == index) {
-      btns.push({
+    if (i == index) {
+      console.log("emoji index", emoji);
+      rows[rows.length - 1].push({
         text: emoji_,
         callback_data: "emoji-" + i + "-" + finishList[i],
       });
-
-    } 
-    
-    else if (unblockList.includes(finishList[i])){
-      btns.push({
+    } else if (unblockList.includes(finishList[i])) {
+      rows[rows.length - 1].push({
         text: finishList[i],
         callback_data: "emoji-" + i + "-" + finishList[i],
       });
-
+    } else {
+      rows[rows.length - 1].push({
+        text: "⬜️",
+        callback_data: "emoji-" + i + "-" + finishList[i],
+      });
     }
-    
-
-    
-    else {
-    btns.push({
-      text: "⬜️",
-      callback_data: "emoji-" + i + "-" + finishList[i],
-    });
   }
-}
-
-
-
-
-
-
-
-  for (let i = amountCards; i < amountCards*2; i++) {
-     if (i == index) {
-       btns2.push({
-         text: emoji_,
-         callback_data: "emoji-" + i + "-" + finishList[i],
-       });
-     } else if (unblockList.includes(finishList[i])) {
-       btns2.push({
-         text: finishList[i],
-         callback_data: "emoji-" + i + "-" + finishList[i],
-       });
-     } else {
-       btns2.push({
-         text: "⬜️",
-         callback_data: "emoji-" + i + "-" + finishList[i],
-       });
-     }
-  }
-
 
   emojiKeyboard = {
     reply_markup: {
-      inline_keyboard: [btns, btns2],
+      inline_keyboard: rows,
     },
   };
-  // console.log(emojiKeyboard.reply_markup.inline_keyboard);
-  
-  let amountWhite = 0
 
-  if (emojiKeyboard.reply_markup != undefined) {
-    let keyboardNew = emojiKeyboard.reply_markup.inline_keyboard.flat();
-    for (let i = 0; i < keyboardNew.length; i++) {
-      if (
-        keyboardNew[i].text == "⬜️")
-       {
-        amountWhite++
+  let amountWhite = emojiKeyboard.reply_markup.inline_keyboard
+    .flat()
+    .filter((btn) => btn.text === "⬜️").length;
+
+  if (amountWhite === 0) {
+    let newRecord = false;
+    let isContainsUserRecord = false;
+    let time = Number((new Date().getTime() / 1000 - startTimeGame).toFixed(0));
+    let index = -1;
+    let oldRecord = 1000000000;
+
+    for (let i = 0; i < records.length; i++) {
+      if (nameUser == records[i].name && records[i].level == amountCards - 2) {
+        isContainsUserRecord = true;
+        index = i;
+        oldRecord = records[i].record;
       }
     }
+
+    if (isContainsUserRecord) {
+      console.log("time", time);
+      console.log("oldRecord", oldRecord);
+      if (time < oldRecord) {
+        bot.sendMessage(chatId, `<b>New Record!</b> You time: ${time}`, {
+          parse_mode: "HTML",
+        });
+
+        let day = new Date().getDate();
+        let month = new Date().getMonth() + 1;
+        let year = new Date().getUTCFullYear();
+        records[index] = {
+          date: `${day}.${month}.${year}`,
+          record: time,
+          level: amountCards - 2,
+          name: nameUser,
+        };
+        saveRecord();
+      }
+    } else {
+      let day = new Date().getDate();
+      let month = new Date().getMonth() + 1;
+      let year = new Date().getUTCFullYear();
+      if (month < 10) {
+        month = "0" + month.toString();
+      }
+      records.push({
+        date: `${day}.${month}.${year}`,
+        record: time,
+        level: amountCards - 2,
+        name: nameUser,
+      });
+      saveRecord();
+    }
+
+    if (amountCards == 22) {
+      bot.sendMessage(chatId, `You Win! Ви пройшли гру!`);
+    } else {
+      bot.sendMessage(
+        chatId,
+        `You Win! You time: ${time}\n /records /start /level`
+      );
+      amountCards++;
+    }
   }
-  
-  if (amountWhite == 0){
-    bot.sendMessage (chatId, 'You Win')
+
+  if (numMessage >= 0) {
+    // bot.answerCallbackQuery({
+    //   callback_query_id: numMessage,
+    // });
   }
-
-
-  
-
-
 
   return emojiKeyboard;
-
 }
-
 
 // /uusername - > italic username reg
 let mainKeyboard = {
@@ -190,91 +236,228 @@ let mainKeyboard = {
   },
 };
 
+bot.on("message", function (message) {
+  let text = message.text;
+  chatId = message.chat.id; // 957345345
+  nameUser = message.chat.first_name;
+  let name = message.chat.first_name;
+  let lastname = message.chat.last_name;
+  let username = message.chat.username;
+  if (text == "/start") {
+    emojiKeyboard = {};
+    finishList = [];
+    unblockList = [];
+    bot.sendMessage(
+      chatId,
+      `<b> start game, you level ${amountCards - 2}</b>`,
+      {
+        parse_mode: "HTML",
+        ...randomKeyboard(),
+      }
+    );
+    startTimeGame = new Date().getTime() / 1000; // 1703868016757   - amount seconds from 1 jan 1970 year
 
-bot.on('message', function (message) {
-    let text = message.text;
-    chatId = message.chat.id; // 957345345
-    let name = message.chat.first_name;
-    let lastname = message.chat.last_name;
-    let username = message.chat.username;
-    if (text == "/start"){
-       emojiKeyboard = {};
-       finishList = [];
-       unblockList = [];
+    // bot.sendMessage(
+    //   chatId,
+    //   "<b>Hello,</b> <i> " +
+    //     name +
+    //     "</i> <u>" +
+    //     lastname +
+    //     "</u>  <s> strike</s> " +
+    //     username +
+    //     "! <a href= 'https://google.com'>link</a>   ",
+    //   {
+    //     parse_mode: "HTML",
+    //     ...randomKeyboard(),
+    //   }
+    // );
+  }
+  if (text == "/level") {
+    bot.sendMessage(chatId, "change level", levelKeyboard);
+  }
+  if (text == "/records") {
+    let text = "";
+
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].level == amountCards - 2) {
+        text += `
+<b>🧑 ${records[i].name} ${records[i].record}s </b> 
+     <code>${records[i].date}</code>
+      `;
+      }
+    }
+
+    if (text == "") {
+      text = "рекордів не знайдено";
+    }
+    bot.sendMessage(chatId, text, {
+      parse_mode: "HTML",
+      ...recordsKeyboard(amountCards - 2),
+    });
+  }
+  if (text == "hello") {
+    bot.sendMessage(chatId, "hi");
+  }
+
+  if (text == "/username") {
+    bot.sendMessage(chatId, "<i> " + username + "</i>", {
+      parse_mode: "HTML",
+    });
+  }
+
+  if (text == 2) {
+    bot.sendMessage(chatId, "<b>4</b>", {
+      parse_mode: "HTML",
+    });
+  }
+});
+
+bot.on("callback_query", async function (message) {
+  nameUser = message.message.chat.first_name;
+  numMessage = message.id;
+  chatId = message.message.chat.id; // 957345345
+
+  if (message.data.startsWith("setLevel")) {
+    let level = message.data.slice(9);
+    amountCards = +level + 2;
+    console.log(amountCards);
+    let maxLevelPlayer = 1;
+
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].name == nameUser && records[i].level > maxLevelPlayer) {
+        maxLevelPlayer = records[i].level + 1;
+      }
+    }
+    if (level <= maxLevelPlayer) {
+      bot.sendMessage(chatId, `рівень ${level} встановлено`);
+    } else {
       bot.sendMessage(
         chatId,
-        "<b> start game</b>",
-        {
-          parse_mode: "HTML",
-          ...randomKeyboard(),
-        }
+        `Вам треба пройти рівень для того щоб його вибрати, ви можете вибрати рівень до ${maxLevelPlayer}`
       );
-        // bot.sendMessage(
-        //   chatId,
-        //   "<b>Hello,</b> <i> " +
-        //     name +
-        //     "</i> <u>" +
-        //     lastname +
-        //     "</u>  <s> strike</s> " +
-        //     username +
-        //     "! <a href= 'https://google.com'>link</a>   ",
-        //   {
-        //     parse_mode: "HTML",
-        //     ...randomKeyboard(),
-        //   }
-        // );
     }
-    if (text == "hello"){
-      bot.sendMessage(chatId, "hi")
+    //  bot.sendMessage(chatId, `рівень ${level} встановлено`);
+  }
+  if (message.data.startsWith("records-num")) {
+    let n = message.data.slice(12);
+    let text = "";
+
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].level == n) {
+        text += `
+<b>🧑 ${records[i].name} ${records[i].record}s </b> 
+     <code>${records[i].date}</code>
+      `;
+      }
     }
 
-     if (text == "/username"){
-        bot.sendMessage(
-          chatId,
-          "<i> " + username +"</i>",
-          {
-            parse_mode: "HTML",
-          }
-        );
-     }
+    if (text == "") {
+      text = "рекордів не знайдено";
+    }
 
-    if (text == 2){
-        bot.sendMessage(
-            chatId,
-        "<b>4</b>", {
-            parse_mode: "HTML",
+    bot.editMessageText(`${text}`, {
+      ...recordsKeyboard(+n),
+      chat_id: chatId,
+      message_id: message.message.message_id,
+      parse_mode: "HTML",
+    });
+  }
+  if (message.data.startsWith("records-right")) {
+    let n = message.data.slice(14);
+
+    if (n >= 25) {
+      bot.answerCallbackQuery(message.id, {
+        text: "error",
+        show_alert: false,
+      });
+    } else {
+      // bot.sendMessage(chatId, "text", recordsKeyboard(+n));
+
+      let text = "";
+
+      for (let i = 0; i < records.length; i++) {
+        if (records[i].level == n) {
+          text += `
+<b>🧑 ${records[i].name} ${records[i].record}s </b> 
+     <code>${records[i].date}</code>
+      `;
         }
-        );
+      }
 
+      if (text == "") {
+        text = "рекордів не знайдено";
+      }
+
+      bot.editMessageText(text, {
+        ...recordsKeyboard(+n),
+        chat_id: chatId,
+        message_id: message.message.message_id,
+        parse_mode: "HTML",
+      });
     }
-})
+  }
+  if (message.data.startsWith("records-left")) {
+    let n = message.data.slice(13);
+    console.log("n", n);
+    if (n <= 3) {
+      n = 1;
+    } else {
+      n = n - 3;
+    }
+    if (n == 0) {
+      bot.answerCallbackQuery(message.id, { text: "error", show_alert: false });
+    } else {
+      let text = "";
 
+      for (let i = 0; i < records.length; i++) {
+        if (records[i].level == n) {
+          text += `
+<b>🧑 ${records[i].name} ${records[i].record}s </b> 
+     <code>${records[i].date}</code>
+      `;
+        }
+      }
 
-bot.on('callback_query', function (message) {
-  chatId = message.message.chat.id; // 957345345
+      if (text == "") {
+        text = "рекордів не знайдено";
+      }
+
+      bot.editMessageText(text, {
+        ...recordsKeyboard(+n),
+        chat_id: chatId,
+        message_id: message.message.message_id,
+        parse_mode: "HTML",
+      });
+    }
+  }
+
   if (message.data == "firstBtn") {
     bot.sendMessage(chatId, "hello 1");
-
   } else if (message.data == "thirdBtn") {
     bot.sendMessage(chatId, "hello 3");
-
   } else if (message.data.startsWith("emoji-")) {
-    let emoji = message.data.slice(8)
-    let indexClickedBtn = message.data.slice(6, 7);
+    // emoji-12-:)     !60letter
+    let emoji = message.data.slice(message.data.length - 2);
+    console.log("emoji", emoji);
+    let indexClickedBtn = message.data.split("-")[1];
     console.log("indexClickedBtn ", indexClickedBtn);
-    
-    
     // let newkeyboard = {
     //   reply_markup: {
     //     inline_keyboard: [[{ text: "my btn", callback_data: "click" }]],
     //   },
     // };
-    bot.editMessageReplyMarkup(randomKeyboard(indexClickedBtn, emoji).reply_markup, {
-      chat_id: message.message.chat.id,
-      message_id: message.message.message_id,
-    });
+    if (numMessage >= 0) {
+      //  await bot.answerCallbackQuery({
+      //    callback_query_id: numMessage,
+      //  });
+    }
+    bot.editMessageReplyMarkup(
+      randomKeyboard(indexClickedBtn, emoji).reply_markup,
+      {
+        chat_id: message.message.chat.id,
+        message_id: message.message.message_id,
+      }
+    );
     //bot.sendMessage(chatId, "emoji: " + emoji);
   }
-  
-})
-
+});
--- keybords.js
+++ keybords.js
@@ -1,9 +1,66 @@
 let mainKeyboard = {
-    reply_markup:{
-        inline_keyboard: [
-            [
-            {text: 'my btn', callback_data: 'click'}
-            ]
-        ]
-    }
+  reply_markup: {
+    inline_keyboard: [[{ text: "my btn", callback_data: "click" }]],
+  },
+};
+
+// 6 (4) ✅
+export function recordsKeyboard(num = 1) {
+  let btns = [];
+  if (num <= 0){
+    num = 1
+  }
+  for (let i = num; i < num + 4; i++) {
+      if (i == num) {
+        btns.push({ text: `${i} ✅`, callback_data: `records-num-${i}` });
+      } else {
+        btns.push({ text: `${i} ⬜️`, callback_data: `records-num-${i}` });
+      }
+  }
+  return {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "⬅️", callback_data: `records-left-${num-1}` },
+          ...btns,
+          { text: "➡️", callback_data: `records-right-${num+4}` },
+        ],
+      ],
+    },
+  };
 }
+
+export let levelKeyboard = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        { text: "1", callback_data: "setLevel-1" },
+        { text: "2", callback_data: "setLevel-2" },
+        { text: "3", callback_data: "setLevel-3" },
+        { text: "4", callback_data: "setLevel-4" },
+        { text: "5", callback_data: "setLevel-5" },
+      ],
+      [
+        { text: "6", callback_data: "setLevel-6" },
+        { text: "7", callback_data: "setLevel-7" },
+        { text: "8", callback_data: "setLevel-8" },
+        { text: "9", callback_data: "setLevel-9" },
+        { text: "10", callback_data: "setLevel-10" },
+      ],
+      [
+        { text: "11", callback_data: "setLevel-11" },
+        { text: "12", callback_data: "setLevel-12" },
+        { text: "13", callback_data: "setLevel-13" },
+        { text: "14", callback_data: "setLevel-14" },
+        { text: "15", callback_data: "setLevel-15" },
+      ],
+      [
+        { text: "16", callback_data: "setLevel-16" },
+        { text: "17", callback_data: "setLevel-17" },
+        { text: "18", callback_data: "setLevel-18" },
+        { text: "19", callback_data: "setLevel-19" },
+        { text: "20", callback_data: "setLevel-20" },
+      ],
+    ],
+  },
+};
